47bf596b0035514ca7cf3e82a0cf1bf716c3f4c6
Ax-Sch <axel.schmidt1@rwth-aachen.de>
2025-04-01 11:52:38 +0200

    added a space, so that trait names can start with p1

--- notebooks/gather_genetic_correlations.sh
+++ notebooks/gather_genetic_correlations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat ../results/ldsc/rg/* | grep "Summary of Gene" -A2 | grep -v "Summary of" | grep -v "\--" | grep "p1" | head -n1 > data_extract.tsv 
-cat ../results/ldsc/rg/* | grep "Summary of Gene" -A2 | grep -v "Summary of" | grep -v "\--" | grep -v "p1" >> data_extract.tsv
+cat ../results/ldsc/rg/* | grep "Summary of Gene" -A2 | grep -v "Summary of" | grep -v "\--" | grep "p1 " | head -n1 > data_extract.tsv 
+cat ../results/ldsc/rg/* | grep "Summary of Gene" -A2 | grep -v "Summary of" | grep -v "\--" | grep -v "p1 " >> data_extract.tsv
 
 
